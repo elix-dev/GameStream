7a16166e83279d9d04bf7d16507ea5860b8486cc
elix-dev <develoelix@gmail.com>
2022-03-28 07:19:06 -0500

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by Elias Jose Rodriguez Indaburo on 28/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
